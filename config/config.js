3710499cbd218c887490d09113d82a915290883c
Justino Soares <justinocsoares123@gmail.com>
2025-01-24 11:14:37 +0100

    last

--- config/config.js
+++ config/config.js
@@ -17,19 +17,33 @@ module.exports = {
     },
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT,
+    timezone: "+01:00", // Luanda está em UTC+1
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true, // Use true em produção
+      },
+    },
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_PROD,
-    host: process.env.DB_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT,
+    timezone: "+01:00", // Luanda está em UTC+1
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true, // Use true em produção
+      },
+    },
   },
 };
